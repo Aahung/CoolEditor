df6dab1bde350b33ffcdbe1caf74cf00554edb8f
aaHung <landxh@gmail.com>
2014-04-17 12:26:55 +0800

    add collections of modes and themes

--- Options.xaml.cs
+++ Options.xaml.cs
@@ -16,17 +16,21 @@ namespace CoolEditor
         {
             InitializeComponent();
             // initialize mode selector
-            var modeSource = new List<Items>
+            var modeNames = new String[]{"abap", "actionscript", "ada", "apache_conf", "asciidoc", "assembly_x86", "autohotkey", "batchfile", "c9search", "c_cpp", "cirru", "clojure", "cobol", "coffee", "coldfusion", "csharp", "css", "curly", "d", "dart", "diff", "django", "dot", "ejs", "erlang", "forth", "ftl", "gherkin", "glsl", "golang", "groovy", "haml", "handlebars", "haskell", "haxe", "html", "html_completions", "html_ruby", "ini", "jack", "jade", "java", "javascript", "json", "jsoniq", "jsp", "jsx", "julia", "latex", "less", "liquid", "lisp", "livescript", "logiql", "lsl", "lua", "luapage", "lucene", "makefile", "markdown", "matlab", "mel", "mushcode", "mushcode_high_rules", "mysql", "nix", "objectivec", "ocaml", "pascal", "perl", "pgsql", "php", "plain_text", "powershell", "prolog", "properties", "protobuf", "python", "r", "rdoc", "rhtml", "ruby", "rust", "sass", "scad", "scala", "scheme", "scss", "sh", "sjs", "smarty", "snippets", "soy_template", "space", "sql", "stylus", "svg", "tcl", "tex", "text", "textile", "toml", "twig", "typescript", "vbscript", "velocity", "verilog", "vhdl", "xml", "xquery", "yaml"};
+            var modeSource = new List<Items>();
+            foreach (var modeName in modeNames)
             {
-                new Items() {Name = "javascript"},
-                new Items() {Name = "c_cpp"},
+                // add all to source
+                modeSource.add(new Items() {Name = modeName}); 
             };
             this.ModePicker.ItemsSource = modeSource;
             // initialize theme selector
-            var themeSource = new List<Items>
+            var themeNames = new String[]{"ambiance", "chaos", "chrome", "clouds", "clouds_midnight", "cobalt", "crimson_editor", "dawn", "dreamweaver", "eclipse", "github", "idle_fingers", "katzenmilch", "kr", "kuroir", "merbivore", "merbivore_soft", "mono_industrial", "monokai", "pastel_on_dark", "solarized_dark", "solarized_light", "terminal", "textmate", "tomorrow", "tomorrow_night", "tomorrow_night_blue", "tomorrow_night_bright", "tomorrow_night_eighties", "twilight", "vibrant_ink", "xcode"};
+            var themeSource = new List<Items>();
+            foreach (var themeName in themeNames)
             {
-                new Items() {Name = "monokai"},
-                new Items() {Name = "terminal"}
+                // add all to source
+                themeSource.add(new Items() {Name = themeName}); 
             };
             this.ThemePicker.ItemsSource = themeSource;
         }
